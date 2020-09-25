ba1eca5b462151f9cbb3f0b96ce6bd4b39f88faf
brian <brianra2nil@hotmail.com>
2020-09-25 01:14:44 -0700

    updated with reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+import Home from './pages/Home'
+import Saved from './pages/Saved'
 
 function App() {
   return (
-    <h1>Hello World!</h1>
-  
+    
+  <Router>
+<div>
+<p>Google API Book Search</p>
+      <Nav>
+        <NavItem>
+          <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/saved">Saved</NavLink>
+        </NavItem>
+      </Nav>
+      <hr />
+     
+<Switch>
+<Route exact path="/" component={Home} />
+<Route path="/saved" component={Saved} />
+</Switch>
+</div>
+</Router>
   )
 }
 
-export default App;
+export default App
