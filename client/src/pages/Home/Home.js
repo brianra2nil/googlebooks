0d650d3e5942a321569c91097d169b8b219283b2
brian <brianra2nil@hotmail.com>
2020-09-25 02:23:18 -0700

    connect front and back

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Book from '../../../../models/Book'
+import MediaContext from '../../utils/MediaContext'
+import Typography from '@material-ui/core/Typography'
+// import Form from '../../components/Form'
+// import Media from '../../components/Book'
+import API from '../../utils/API'
 
 
 const Home = () => {
+
+    const [bookState, setBookState] = useState({
+        search: '', // must be the same as input's name attribute 
+        book: []
+      })
+
+      bookState.handleInputChange = event => {
+        setBookState({ ...bookState, [event.target.name]: event.target.value })
+      }
+
+      bookState.handleSearchGoogle = event => {
+        event.preventDefault()
+        API.getBook(bookState.search)
+          .then(({ data }) => {
+            setBookState({ ...bookState, book: data, search: '' })
+          })
+          .catch(err => console.error(err))
+      
+      }
+
+      bookState.handleSaveBook = googleId => {
+        const saveBook = bookState.book.filter(x => x.googleId === googleId)[0]
+        API.saveBook(saveBook)
+          .then(() => {
+            const book = bookState.book.filter(x => x.googleId !== googleId)
+            setBookState({ ...bookState, book })
+          })
+      }
+
     return (
-        <h1>This is the Home page</h1>
+        <>
+        <hr />
+        <Typography variant="h6">
+          Search for Books on Google
+        </Typography>
+        <BookContext.Provider value={bookState}>
+        <Form />
+        {
+          bookState.book.length > 0 ? (
+            bookState.book.map(book => (
+              <Book
+                key={book.googleId}
+                book={book}
+                handleSaveBook={bookState.handleSaveBook} />
+            ))
+          ) : null
+        }
+        </BookContext.Provider>
+      </>
     )
-}
-
-
-export default Home
+  }
+  
+  export default Home
--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,10 +1,47 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import API from '../../utils/API'
 
 const Saved = () => {
-    return (
-        <h1>This is the Saved page</h1>
-    )
+
+  const [savedState, setSavedState] = useState({
+    saved: []
+  })
+
+  savedState.handleDeleteSaved = id => {
+    API.deleteBook(id)
+      .then(() => {
+        let saved = savedState.saved.filter(book => book._id !== id)
+        setSavedState({ ...savedState, saved })
+      })
+  }
+
+  useEffect(() => {
+    API.getSavedBook()
+      .then(({ data }) => {
+        setSavedState({ ...savedState, saved: data })
+      })
+  }, [])
+
+  return (
+    <>
+    <h1>Your saved Books</h1>
+      {
+        savedState.saved.length > 0 ? (
+          savedState.saved.map(book => (
+            <div key={book.googleId}>
+              <img src={book.image} alt={book.title} />
+              <h3>{book.title}</h3>
+              <h4>Authors: {book.authors}</h4>
+              <h4>Description: {book.description}</h4>
+              <h5>googleId: {book.googleId}</h5>
+          <h5>link: {book.infoLink}</h5>
+              <button onClick={() => savedState.handleDeleteSaved(book._id)}>Delete</button>
+            </div>
+          ))
+        ) : null
+      }
+    </>
+  )
 }
 
 export default Saved
